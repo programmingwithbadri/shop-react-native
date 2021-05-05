ccc7353da4cdf2615d34e86689e6ecb8e5379a4d
Badri Narayanan <programmingwithbadri@outlook.com>
2021-05-05 23:16:35 +0530

    Add the cartScreen to have the cart items

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 
 import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
 import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  cart: cartReducer
 });
 
 const store = createStore(rootReducer);
--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -7,11 +7,13 @@ import {
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import Colors from '../constants/Colors';
+import CartScreen from '../screens/shop/CartScreen';
 
 const ProductsNavigator = createStackNavigator(
     {
         ProductsOverview: ProductsOverviewScreen,
         ProductDetail: ProductDetailScreen,
+        Cart: CartScreen
     },
     {
         defaultNavigationOptions: {
