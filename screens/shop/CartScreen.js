81d9eccaed675925d5fdecf2b351bdd3557cf89f
Badri Narayanan <programmingwithbadri@outlook.com>
2021-05-06 21:56:17 +0530

    Removing item from cart

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import * as cartActions from '../../store/actions/cart';
 
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
@@ -18,8 +19,11 @@ const CartScreen = props => {
                 sum: state.cart.items[key].sum
             });
         }
-        return transformedCartItems;
+        return transformedCartItems.sort((a, b) =>
+            a.productId > b.productId ? 1 : -1
+        );
     });
+    const dispatch = useDispatch();
 
     return (
         <View style={styles.screen}>
@@ -42,7 +46,9 @@ const CartScreen = props => {
                         quantity={itemData.item.quantity}
                         title={itemData.item.productTitle}
                         amount={itemData.item.sum}
-                        onRemove={() => { }}
+                        onRemove={() => {
+                            dispatch(cartActions.removeFromCart(itemData.item.productId));
+                        }}
                     />
                 )}
             />
