7893c6f5b198e6c9681274c3879f35fa7439ee76
Badri Narayanan <programmingwithbadri@outlook.com>
2021-05-06 23:03:18 +0530

    Customize the orderScreen to show the users orders

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { FlatList, Text, Platform } from 'react-native';
+import { useSelector } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-const OrdersScreen = () => {
+import HeaderButton from '../../components/UI/HeaderButton';
+import OrderItem from '../../components/shop/OrderItem';
+
+const OrdersScreen = props => {
+    const orders = useSelector(state => state.orders.orders);
+
     return (
-        <View>
-            <Text> OrdersScreen </Text>
-        </View>
-    )
-}
+        <FlatList
+            data={orders}
+            keyExtractor={item => item.id}
+            renderItem={itemData => (
+                <OrderItem
+                    amount={itemData.item.totalAmount}
+                    date={itemData.item.readableDate}
+                    items={itemData.item.items}
+                />
+            )}
+        />
+    );
+};
 
-export default OrdersScreen
+OrdersScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'Your Orders',
+        headerLeft: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Menu"
+                    iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+                    onPress={() => {
+                        navData.navigation.toggleDrawer();
+                    }}
+                />
+            </HeaderButtons>
+        )
+    };
+};
+
+export default OrdersScreen;
